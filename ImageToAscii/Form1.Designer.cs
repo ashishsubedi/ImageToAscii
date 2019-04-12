d543af701b1ea991ba85dfbed9d3c1d14e7c97dc
Ashish Subedi <ashishsubedi10@outlook.com>
2019-04-12 08:53:31 +0545

    Add project files.

---
+++ ImageToAscii/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ImageToAscii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.data = new System.Windows.Forms.RichTextBox();
+            this.opnBtn = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.path = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.save = new System.Windows.Forms.Button();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // data
+            // 
+            this.data.Location = new System.Drawing.Point(1, 130);
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(621, 575);
+            this.data.TabIndex = 0;
+            this.data.Text = "";
+            // 
+            // opnBtn
+            // 
+            this.opnBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.opnBtn.Location = new System.Drawing.Point(19, 33);
+            this.opnBtn.Name = "opnBtn";
+            this.opnBtn.Size = new System.Drawing.Size(105, 29);
+            this.opnBtn.TabIndex = 1;
+            this.opnBtn.Text = "Open";
+            this.opnBtn.UseVisualStyleBackColor = true;
+            this.opnBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Enabled = false;
+            this.convertBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.convertBtn.Location = new System.Drawing.Point(157, 33);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(105, 29);
+            this.convertBtn.TabIndex = 2;
+            this.convertBtn.Text = "Convert";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // path
+            // 
+            this.path.AutoSize = true;
+            this.path.Location = new System.Drawing.Point(19, 69);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(0, 13);
+            this.path.TabIndex = 3;
+            // 
+            // save
+            // 
+            this.save.Enabled = false;
+            this.save.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.save.Location = new System.Drawing.Point(282, 33);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(105, 29);
+            this.save.TabIndex = 5;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(19, 68);
+            this.trackBar.Maximum = 400;
+            this.trackBar.Minimum = 100;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(368, 45);
+            this.trackBar.TabIndex = 6;
+            this.trackBar.Value = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 754);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.opnBtn);
+            this.Controls.Add(this.data);
+            this.Name = "Form1";
+            this.Text = "Image to Ascii";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox data;
+        private System.Windows.Forms.Button opnBtn;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.Label path;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.TrackBar trackBar;
+    }
+}
+
---
+++ ImageToAscii/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageToAscii
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string imagePath = "";
+        int asciiWidth;
+        string _content;
+        private string[] _AsciiChars = { "#", "#", "@", "%", "=", "+", "*", ":", "-", ".", " " };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFile.Filter = "jpg files (*.jpg)|*.jpg|png files (*.png)|*.png|All files (*.*)|*.*";
+
+            DialogResult result = openFile.ShowDialog();
+
+            if(result == DialogResult.OK)
+            {
+                imagePath = path.Text = openFile.FileName;
+                convertBtn.Enabled = true;
+            }
+        }
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+            convertBtn.Enabled = false;
+
+            Bitmap image = new Bitmap(imagePath);
+            asciiWidth = this.trackBar.Value;
+            image = GetResizedImage(image, asciiWidth);
+            _content = ConvertToAscii(image);
+            data.Text = _content;
+            save.Enabled = true;
+
+        }
+
+        private Bitmap GetResizedImage(Bitmap image, int asciiWidth)
+        {
+            int asciiHeight = 0 ;
+
+            asciiHeight = (int)Math.Ceiling((double)image.Width * asciiWidth / image.Height);
+
+            Bitmap newImage = new Bitmap(asciiWidth, asciiHeight);
+            Graphics g = Graphics.FromImage((Image)newImage);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            g.DrawImage(image, 0, 0, asciiWidth, asciiHeight);
+            g.Dispose();
+            return newImage;
+
+        }
+        private string ConvertToAscii(Bitmap image)
+        {
+            //bool toogle = false;
+            //StringBuilder sb = new StringBuilder();
+
+            //for(int h=0; h<image.Height; h++)
+            //{
+            //    for (int w = 0; w < image.Width; w++)
+            //    {
+            //        Color pixelColor = image.GetPixel(w, h);
+            //        int average;
+            //        average = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+
+            //        Color gray = Color.FromArgb(average);
+
+            //        if (!toogle)
+            //        {
+            //            int index = (gray.R * 10) / 255;
+            //            sb.Append(_AsciiChars[index]);
+            //        }
+            //    }
+            //    if (!toogle)
+            //    {
+            //        sb.Append("\n");
+            //        toogle = true;
+            //    }
+            //    else
+            //    {
+            //        toogle = false;
+            //    }
+            //}
+            //return sb.ToString();
+
+            Boolean toggle = false;
+
+            StringBuilder sb = new StringBuilder();
+
+
+
+            for (int h = 0; h < image.Height; h++)
+
+            {
+
+                for (int w = 0; w < image.Width; w++)
+
+                {
+
+                    Color pixelColor = image.GetPixel(w, h);
+
+                    //Average out the RGB components to find the Gray Color
+
+                    int red = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+
+                    int green = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+
+                    int blue = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+
+                    Color grayColor = Color.FromArgb(red, green, blue);
+
+
+
+                    //Use the toggle flag to minimize height-wise stretch
+
+                    if (!toggle)
+
+                    {
+
+                        int index = (grayColor.R * 10) / 255;
+
+                        sb.Append(_AsciiChars[index]);
+
+                    }
+
+                }
+
+                if (!toggle)
+
+                {
+
+                    sb.Append("\n");
+
+                    toggle = true;
+
+                }
+
+                else
+
+                {
+
+                    toggle = false;
+
+                }
+
+            }
+
+            return sb.ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Text File (*.txt)|.txt";
+
+            DialogResult diag = saveFileDialog1.ShowDialog();
+
+            if (diag == DialogResult.OK)
+
+            {              
+
+                StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+
+                sw.Write(_content);
+
+                sw.Flush();
+
+                sw.Close();
+
+            }
+        }
+    }
+}
